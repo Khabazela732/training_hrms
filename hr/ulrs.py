2dd9aa96aa152f13fcd22236acdfafc97a246ccd
Prenett <prenetprincess@gmail.com>
2026-03-03 10:37:06 +0200

    Creat, View, Update, Delete and status change

--- hr/ulrs.py
+++
@@ -1,8 +0,0 @@
-from .views import DashboardView, EmployeesView, LeaveListView
-from django.urls import path
-
-urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),
-    path('employees/', EmployeesView.as_view(), name='employees'),
-    path('leaves/', LeaveListView.as_view(), name='leave-list')
-]
--- hr/views.py
+++ hr/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
 from django.views import View
 from .models import Employee, Department, Leave
+from .forms import LeaveForm
 
-#my changes
 
 class DashboardView(View):
     def get(self, request):
@@ -16,6 +16,7 @@ class DashboardView(View):
         }
         return HttpResponse(template.render(context, request))
 
+
 class EmployeesView(View):
     def get(self, request):
         template = loader.get_template('hr/pages/employees.html')
@@ -23,16 +24,69 @@ class EmployeesView(View):
         context = {
             'employees': employees,
             'department_count': Department.objects.count(),
-           
         }
         return HttpResponse(template.render(context, request))
+
 
 class LeaveListView(View):
     def get(self, request):
         template = loader.get_template('hr/pages/leave.html')
         leaves = Leave.objects.all()
         context = {
-            'leaves' : leaves
+            'leaves': leaves
         }
-
         return HttpResponse(template.render(context, request))
+
+# List all leaves
+def leave_list(request):
+    leaves = Leave.objects.all()
+    return render(request, 'hr/leave/list.html', {'leaves': leaves})
+
+# View leave detail
+def leave_detail(request, pk):
+    leave = get_object_or_404(Leave, pk=pk)
+    return render(request, 'hr/leave/detail.html', {'leave': leave})
+
+# Update leave
+def leave_update(request, pk):
+    leave = get_object_or_404(Leave, pk=pk)
+    if request.method == 'POST':
+        form = LeaveForm(request.POST, instance=leave)
+        if form.is_valid():
+            form.save()
+            return redirect('leave-list')
+    else:
+        form = LeaveForm(instance=leave)
+    return render(request, 'hr/leave/update.html', {'form': form})
+
+# Delete leave
+def leave_delete(request, pk):
+    leave = get_object_or_404(Leave, pk=pk)
+    if request.method == 'POST':
+        leave.delete()
+        return redirect('leave-list')
+    return render(request, 'hr/leave/delete.html', {'leave': leave})
+
+# Create leave
+def leave_create(request):
+    if request.method == 'POST':
+        form = LeaveForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = LeaveForm()
+    return render(request, 'hr/leave/create.html', {'form': form})
+
+#staus functions
+def leave_approve(request, id):
+    leave = get_object_or_404(Leave, id=id)
+    leave.status = "Approved"
+    leave.save()
+    return redirect('leave-list')
+
+
+def leave_decline(request, id):
+    leave = get_object_or_404(Leave, id=id)
+    leave.status = "Declined"
+    leave.save()
+    return redirect('leave-list')
