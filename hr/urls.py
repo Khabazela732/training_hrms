db2f4c16b23fc81704aada90833797b260389487
FranceMokoena <developer091120@gmail.com>
2026-03-04 08:31:39 +0200

    From The Updated Main Repo

--- hr/urls.py
+++ hr/urls.py
@@ -1,10 +1,14 @@
-from .views import DashboardView, EmployeesView, Attendance_View, LeaveListView, DepartmentsView, DepartmentCreateView, DepartmentUpdateView, DepartmentDeleteView, PayrollView, AddPayrollView, EditPayrollView, DeletePayrollView, ViewPayrollReport
+from .views import DashboardView, EmployeesView, EmployeeCreateView, EmployeeUpdateView, EmployeeDeleteView, EmployeeDetailView, Attendance_View, LeaveListView, DepartmentsView, DepartmentCreateView, DepartmentUpdateView, DepartmentDeleteView, PayrollView, AddPayrollView, EditPayrollView, DeletePayrollView, ViewPayrollReport
 from django.urls import path
 from . import views 
-
+#France
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
     path('employees/', EmployeesView.as_view(), name='employees'),
+    path('employees/create/', EmployeeCreateView.as_view(), name='employee_create'),
+    path('employees/<int:pk>/', EmployeeDetailView.as_view(), name='employee_detail'),
+    path('employees/<int:pk>/edit/', EmployeeUpdateView.as_view(), name='employee_update'),
+    path('employees/<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee_delete'),
     path('leaves/', LeaveListView.as_view(), name='leave-list'),
     
     # Sethu's code
