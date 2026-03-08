7e6a2420502d31bfe597da159ee7dc104a2da4af
Zee2468 <prudencezandile77@gmail.com>
2026-03-08 20:56:36 +0200

    payroll-bulk upload

--- hr/admin.py
+++ hr/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Department, Role, Employee, Leave, Attendance
+from .models import Department, Role, Employee, Leave, Attendance, Payroll
 
 admin.site.register(Department)
 admin.site.register(Role)
 admin.site.register(Employee)
 admin.site.register(Leave)
 admin.site.register(Attendance)
+admin.site.register(Payroll)
